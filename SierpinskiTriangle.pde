35ff82b2669217684cb9b1201d0187c27d6f58fc
luchen825 <lucychen28@gmail.com>
2015-01-14 10:53:43 -0800

    changed

--- SierpinskiTriangle.pde
+++ SierpinskiTriangle.pde
@@ -14,7 +14,7 @@ public void mouseDragged()//optional
 }
 public void sierpinski(int x, int y, int len) 
 {
-	if(len > 100)
+	if(len > 20)
 	{
 		sierpinski(x, y, len/2);
 		sierpinski(x + len/2, y, len/2);
--- build-tmp/source/SierpinskiTriangle.java
+++ build-tmp/source/SierpinskiTriangle.java
@@ -18,11 +18,10 @@ public void setup()
 {
 	size(500,500);
 	background(8,1,18);
-	fill((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
 }
 public void draw()
 {
-	//fill(226,218,240);
+	fill(226,218,240);
 	sierpinski(50,450,400);
 }
 public void mouseDragged()//optional
@@ -31,7 +30,7 @@ public void mouseDragged()//optional
 }
 public void sierpinski(int x, int y, int len) 
 {
-	if(len > 100)
+	if(len > 20)
 	{
 		sierpinski(x, y, len/2);
 		sierpinski(x + len/2, y, len/2);
